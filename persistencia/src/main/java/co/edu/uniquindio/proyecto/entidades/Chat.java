eb4b260eb66a9ebfd7de88ce00f4dd86295a9d87
tarttaros <jaosoriop_2@uqvirtual.edu.co>
2022-10-18 18:15:05 -0500

    se añadio la etiqueta @Entity en chat.

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Chat.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Chat.java
@@ -5,6 +5,7 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
---
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Message.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.proyecto.entidades;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Message
+{
+
+
+}
