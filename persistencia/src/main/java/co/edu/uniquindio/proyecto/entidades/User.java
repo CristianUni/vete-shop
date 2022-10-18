c241d9709843c387495f8064235d28c1d2890204
CristianUni <caguevarar@uqvirtual.edu.co>
2022-10-18 16:55:21 -0500

    creación entidades persona, usuario y veterinario

---
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/User.java
@@ -0,0 +1,28 @@
+package co.edu.uniquindio.proyecto.entidades;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+//Entidad hijo Usuario
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+
+public class User extends Person implements Serializable {
+
+    //Llave primaria de la entidad
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+
+
+}
---
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Veterinary.java
@@ -0,0 +1,30 @@
+package co.edu.uniquindio.proyecto.entidades;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+//Entidad hijo veterinario
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class Veterinary extends Person implements Serializable {
+
+    //Llave primaria de la entidad
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_veterinary")
+    private Integer id;
+
+    //Atributo numero de la tarjeta profesional del veterinario
+    @Column(nullable = false,length = 13)
+    @Length(min = 5, max = 13, message = "La tarjeta profesional debe tener mínimo 5 caracteres y máximo 13")
+    @NotBlank(message = "El campo está vacío, debe ingresar la tarjeta profesional")
+    private String num_license;
+}
