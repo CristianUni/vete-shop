dfb610a64beb936518b90827f147dffc4fa35e6f
tarttaros <jaosoriop_2@uqvirtual.edu.co>
2022-10-18 19:54:29 -0500

    se añadio la entidad mensaje y se establecio la relacion con la entidad chat

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Message.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Message.java
@@ -1,8 +1,12 @@
 package co.edu.uniquindio.proyecto.entidades;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -11,8 +15,27 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
-public class Message
+public class Message implements Serializable
 {
+    //Llave primaria de la entidad
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_message")
+    private Integer id;
 
+    //Atributo mensaje que se envia
+    @Column(nullable = false,length = 500)
+    @Length(min = 2, max = 500, message = "El mensaje debe tener mínimo 1 caracteres y máximo 500")
+    @NotBlank(message = "El campo está vacío, ingresar un mensaje a enviar")
+    private String message;
 
+    //Atributo fecha del mensaje
+    @Column(nullable = false)
+    private LocalDate date;
+
+    //Atributo chat al que pertenece el mensaje
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_chat")
+    Chat chat;
 }
